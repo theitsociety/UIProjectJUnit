5769509015eefb11b9a846f0641ba825ce108644
esra <esra_izgi@hotmail.com>
2022-02-01 23:20:21 -0800

    created TC09_EsraIpage and TC09_EsraItest

---
+++ src/test/java/pages/TC09_EsraIpage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class TC09_EsraIpage {
+
+
+    public TC09_EsraIpage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+   // @FindBy(className ="logo pull-left")
+    @FindBy(xpath ="//div[@class='logo pull-left']")
+    public WebElement automationExerciseText;
+
+    @FindBy(xpath="(//li)[2]")
+    public WebElement products;
+
+    @FindBy(xpath="//h2[contains(text(),'All Products')]")
+    public WebElement allProducts;
+
+    @FindBy(xpath="//input[@class= 'form-control input-lg']")
+    public WebElement searchBox;
+
+    @FindBy(xpath="//i[@class= 'fa fa-search']")
+    public WebElement searchButton;
+
+    @FindBy(xpath="//h2[contains(text(),'Searched Products')]")
+    public WebElement searchedProducts;
+
+}
+
---
+++ src/test/java/tests/TC09_EsraItest.java
@@ -0,0 +1,48 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import pages.TC09_EsraIpage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class TC09_EsraItest{
+/*
+    1. Launch browser
+    2. Navigate to url 'http://automationexercise.com'
+    3. Verify that home page is visible successfully
+    4. Click on 'Products' button
+    5. Verify user is navigated to ALL PRODUCTS page successfully
+    6. Enter product name in search input and click search button
+    7. Verify 'SEARCHED PRODUCTS' is visible
+    8. Verify all the products related to search are visible
+     */
+
+
+    TC09_EsraIpage tc09_esraIpage;
+
+    @Test
+    public void SearchProduct(){
+        tc09_esraIpage = new TC09_EsraIpage();
+        Driver.getDriver().get(ConfigurationReader.getProperty("automation_Exercise_url"));
+
+        boolean text = tc09_esraIpage.automationExerciseText.isDisplayed();
+        Assert.assertTrue(text);
+        tc09_esraIpage.products.click();
+
+        String allProductsText =tc09_esraIpage.allProducts.getText();
+        Assert.assertEquals(allProductsText,"ALL PRODUCTS");
+
+        tc09_esraIpage.searchBox.sendKeys("Blue Top");
+        tc09_esraIpage.searchButton.click();
+
+        String searchProducts= tc09_esraIpage.searchedProducts.getText();
+        Assert.assertEquals(searchProducts, "SEARCHED PRODUCTS");
+
+
+
+
+    }
+
+
+}
