a85c6ca3deff838fc310b2c5cf4f2f007107822e
esra <esra_izgi@hotmail.com>
2022-02-04 20:00:05 -0800

    added TC10_EsraItest

---
+++ src/test/java/tests/TC10_EsraItest.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.junit.Test;
+
+public class TC10_EsraItest {
+
+    /*
+    1. Launch browser
+    2. Navigate to url 'http://automationexercise.com'
+    3. Verify that home page is visible successfully
+    4. Scroll down to footer
+    5. Verify text 'SUBSCRIPTION'
+    6. Enter email address in input and click arrow button
+    7. Verify success message 'You have been successfully subscribed!' is visible
+     */
+
+    @Test
+    public void VerifySubscriptionInHomePage() {
+
+
+    }
+}
